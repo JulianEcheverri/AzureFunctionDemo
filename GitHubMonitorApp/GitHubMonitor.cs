0795dcef4d82d959a931c5e0a679b383a2e39f01
Julian David Echeverri Garcia <julian.echeverri@siigo.com>
2022-03-07 14:51:45 -0500

    sample function

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -14,22 +14,17 @@ namespace GitHubMonitorApp
         // Default route azure_url/api/{function_name}
         [FunctionName("GitHubMonitor")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Out GitHub Monitor Processed an action");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            log.LogInformation(requestBody);
 
-            return new OkObjectResult(responseMessage);
+            return new OkResult();
         }
     }
 }
